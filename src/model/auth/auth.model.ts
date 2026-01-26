259b33867fe4ba35f4b93042b85fe979aaafb876
eucleniocadete <eucleniocadete@gmail.com>
2026-01-26 05:46:36 +0100

    Login and create farm pass

--- src/model/auth/auth.model.ts
+++ src/model/auth/auth.model.ts
@@ -411,6 +411,158 @@ class AuthModel {
         }
     }
 
+    async createFarmPass(farmId: string,name: string, email: string, phone: string, province: Province, adress: string, pass1: string, pass2: string){
+        const validData = new DataValidate()
+
+        const validNumber = validData.phoneNumber(phone)
+        const validName = validData.name(name)
+        const validEmail = validData.email(email)
+        const isValidPassword = validData.password(pass1)
+
+        if(!validName){
+            return {
+                valid: false,
+                message: "Nome muito curto"
+            }
+        }
+
+        if(pass2 != pass1){
+            return {
+                valid: false,
+                message: "Credencias inválidas"
+            }
+        }
+
+        if(!isValidPassword.valid){
+            return {
+                valid: isValidPassword.valid,
+                error: isValidPassword.error,
+                strong: isValidPassword.strong
+            }
+        }
+        if(!validNumber){
+            return {
+                valid: false,
+                message: "Número de telefone inválido"
+            }  
+
+        }
+        if(!validEmail){ 
+            return {
+                valid: false,
+                message: "Email inválido"
+            }
+        }
+
+        try {
+            const hashClass = new PasswordHash()
+            const hash = await hashClass.generate(pass1)
+            
+            try {
+                const farmRow = await prisma.users.update({
+                    where: {id: farmId},
+                    data: {
+                        name: name,
+                        phone: phone,
+                        province: province,
+                        adress: adress,
+                        status: "active",
+                        password: String(hash)
+                    }
+                })
+
+                if(!farmRow){
+                    return {
+                        valid: false,
+                        message: "Não foi possível definir senha"
+                    }
+                }
+
+                return {
+                    valid: true,
+                    message: "Senha definida com sucesso"
+                }
+            } catch (error) {
+                return {error: "Ocorreu um erro ao definir senha"}
+            }
+        } catch (error) {
+            return {error: "Ocorreu um erro inesperado"}
+        }
+
+    }
+
+    async login(email: string, pass: string){
+        try {
+            const userData = await prisma.users.findUnique({
+                where: {email: email}
+            })
+
+            if(userData){
+                const hashClass = new PasswordHash()
+                const verifyPass = await hashClass.compare(userData.password, pass)
+
+                if(verifyPass){
+                    //Criar token de acesso
+                    const token = jwt.sign(
+                        {userId: userData.id, type: userData.rule},
+                        process.env.SECRET_KEY!,
+                        {expiresIn: "1d"}
+                    )
+
+                    return {
+                        message: "Login efetuado com sucesso",
+                        token: token
+                    }
+                }
+
+            }
+
+            return {
+                valid: false,
+                error: "Email ou senha inválidos"
+            }
+
+        } catch (error) {
+            return {error: "Erro ao efetuar login"}
+        }
+    }
+
+    async farmSignIn(nif: string, pass: string){
+        try {
+            const userData = await prisma.users.findFirst({
+                where: {farms: {some: {nif: nif}}}
+            })
+
+            if(userData){
+                const hashClass = new PasswordHash()
+                const verifyPass = await hashClass.compare(userData.password, pass)
+
+                if(verifyPass){
+                    //Criar token de acesso
+                    const token = jwt.sign(
+                        {userId: userData.id, type: userData.rule},
+                        process.env.SECRET_KEY!,
+                        {expiresIn: "1d"}
+                    )
+
+                    return {
+                        valid: true,
+                        message: "Login efetuado com sucesso",
+                        token: token
+                    }
+                }
+
+            }
+
+            return {
+                valid: false,
+                message: "NIF ou senha inválidos"
+            }
+
+        } catch (error) {
+            return {error: "Erro ao efetuar login"}
+        }
+    }
 }
 
 export { AuthModel }
