af696a4251bf2e785427011ec90c6922275d07f9
eucleniocadete <eucleniocadete@gmail.com>
2026-01-26 05:47:15 +0100

    Login and create farm pass routes

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -1,5 +1,7 @@
 import { Router, type Request, type Response } from "express";
 import authController from "../controllers/auth/auth.controller";
+import { auth } from "../middlewares/auth";
+import { authorization } from "../middlewares/permissions";
 
 const router = Router()
 
@@ -7,5 +9,8 @@ const router = Router()
 router.post('/auth/signup/:rule', authController.signup)
 router.get('/auth/signup/verify/:code', authController.verifyCode)
 router.get('/auth/signup/nif/:nif', authController.verifyNif)
+router.post('/auth/farm/signup', auth, authorization("farm", "create"), authController.createFarmPass)
+router.get('/auth/farm/login', authController.farmSignIn)
+router.get('/auth/login', authController.login)
 
 export { router }
