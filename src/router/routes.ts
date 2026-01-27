f55d74bb0a9d94ff9878a04b93a97bf5e9129eb4
eucleniocadete <eucleniocadete@gmail.com>
2026-01-27 23:46:02 +0100

    Products routes

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -2,6 +2,7 @@ import { Router, type Request, type Response } from "express";
 import authController from "../controllers/auth/auth.controller";
 import { auth } from "../middlewares/auth";
 import { authorization } from "../middlewares/permissions";
+import productsController from "../controllers/products.controller";
 
 const router = Router()
 
@@ -12,5 +13,13 @@ router.get('/auth/signup/nif/:nif', authController.verifyNif)
 router.post('/auth/farm/signup', auth, authorization("farm", "create"), authController.createFarmPass)
 router.get('/auth/farm/login', authController.farmSignIn)
 router.get('/auth/login', authController.login)
+
+//Products
+router.post('/products/create', auth, authorization("farm", "create"), productsController.create)
+router.post('/products/upload/:id', auth, authorization("farm", "create"), productsController.productPhoto)
+router.put('/products/update/:id', auth, authorization("farm", "update"), productsController.update)
+router.get('/products/farms/getAll', auth, authorization("farm", "read"), productsController.getAll)
+router.get('/products/farms/get/:id', auth, authorization("farm", "read"), productsController.get)
+router.delete('/products/delete/:id', auth, authorization("farm", "delete"), productsController.delete)
 
 export { router }
