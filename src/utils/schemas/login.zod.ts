d86fcc735085b617799672e4e484fc4a244b0640
eucleniocadete <eucleniocadete@gmail.com>
2026-01-27 01:05:08 +0100

    undefined key message

--- src/utils/schemas/login.zod.ts
+++ src/utils/schemas/login.zod.ts
@@ -1,11 +1,11 @@
 import z from "zod";
 
 export const loginSchema = z.object({
-    email: z.string().email({message: "Email inválido"}).trim(),
+    email: z.string({message: "Chave email esperada e não foi recebido"}).email({message: "Email inválido"}).trim(),
     password: z.string()
 })
 
 export const farmSigninSchema = z.object({
-    nif: z.string().trim().min(9, "O NIF deve conter 9 dígitos").max(9, "O NIF deve conter 9 dígitos"),
+    nif: z.string({message: "Chave nif esperada e não foi recebido"}).trim().min(9, "O NIF deve conter 9 dígitos").max(9, "O NIF deve conter 9 dígitos"),
     password: z.string()
 })
