a0ee93fef6967ac65b0687aff119e91125124ad4
eucleniocadete <eucleniocadete@gmail.com>
2026-01-27 23:44:41 +0100

    Allow files upload

--- src/app.ts
+++ src/app.ts
@@ -3,10 +3,17 @@ import helmet from "helmet";
 import { router } from "./router/routes";
 import { limiter } from "./config/rate-limiting";
 import morgan from 'morgan'
+import fileupload from 'express-fileupload' 
+import path from "path";
 
 const app = express()
 
 app.use(express.json())
+app.use(fileupload())
+app.use(
+  "/upload",
+  express.static(path.resolve("src/upload"))
+)
 app.use(helmet())
 app.use(limiter)
 app.use(morgan("combined"))
