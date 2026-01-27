29ba58ac8ed381fc7200018f3603766ad7f4cd25
eucleniocadete <eucleniocadete@gmail.com>
2026-01-27 23:51:35 +0100

    Upload file function

---
+++ src/utils/upload.ts
@@ -0,0 +1,24 @@
+import dayjs from "dayjs";
+import type { UploadedFile } from "express-fileupload";
+import path from "node:path";
+
+export interface uploadResult {
+    ok: boolean,
+    filename: string
+}
+
+export async function upload(img: UploadedFile, format: string, url: string, to: "user" | "product"): Promise<uploadResult> {
+    const rename = `${to}-${dayjs().format('DD-MM-YYYY_HHmmss')}.${format}`
+
+    const pathUrl = path.join(url, rename)
+    
+    return new Promise((resolve, reject)=>{
+        img.mv(pathUrl, error =>{
+            if(error){
+                reject("Não foi possível salvar imagem")
+            }else{
+                resolve({ok: true, filename: rename})
+            }
+        })
+    })
+}
