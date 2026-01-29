72ec4fb81ca9eddb4c2670d45306e5f04ba4d610
eucleniocadete <eucleniocadete@gmail.com>
2026-01-29 17:00:58 +0100

    consumer view a product path

---
+++ src/doc/paths/consumer-get-product.ts
@@ -0,0 +1,35 @@
+export const consumerGetAProductPath = {
+    '/products/consumers/farm/{farmId}/product/{id}': {
+        get: {
+            tags: ["Products"],
+            summary: "Consumidor visualiza um produto",
+            parameters: [
+                {
+                    name: "farmId",
+                    in: "path",
+                    required: true,
+                    schema: {
+                        type: "string",
+                        example: "5477da12-e907-4e4c-a4ed-23cadba77b3e"
+                    }
+                },
+                {
+                    name: "id",
+                    in: "path",
+                    required: true,
+                    schema: {
+                        type: "string",
+                        example: "c76766a9-a280-4871-801c-43e8a1346a2b"
+                    }
+                }
+            ],
+            responses: {
+                200: {description: "ÕK"},
+                400: {description: "Bad Request"},
+                401: {description: "Unauthorized"},
+                403: {description: "Forbidden"},
+                404: {description: "Not Found"}
+            }
+        }
+    }
+}
