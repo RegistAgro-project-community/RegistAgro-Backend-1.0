1cdf13b5fe713c3c6c6de7c4a55cd7f69195b99d
eucleniocadete <eucleniocadete@gmail.com>
2026-01-29 02:34:43 +0100

    Add documentation

--- src/app.ts
+++ src/app.ts
@@ -5,14 +5,15 @@ import { limiter } from "./config/rate-limiting";
 import morgan from 'morgan'
 import fileupload from 'express-fileupload' 
 import path from "path";
+import swaggerUi from 'swagger-ui-express'
+import { swaggerSpec } from "./doc/swagger";
 
 const app = express()
 
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec))
 app.use(express.json())
 app.use(fileupload())
-app.use(
-  "/upload",
-  express.static(path.resolve("src/upload"))
+app.use("/upload", express.static(path.resolve("src/upload"))
 )
 app.use(helmet())
 app.use(limiter)
