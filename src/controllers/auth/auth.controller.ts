8f4ead12f28c228ec36177634cb33184bbefaaac
eucleniocadete <eucleniocadete@gmail.com>
2026-01-29 02:36:17 +0100

    response status changed to 201

--- src/controllers/auth/auth.controller.ts
+++ src/controllers/auth/auth.controller.ts
@@ -30,7 +30,7 @@ class AuthController {
                     return res.status(400).json(signupResult)
                 }
 
-                return res.status(200).json(signupResult)
+                return res.status(201).json(signupResult)
 
             } catch (error) {
                 errors(res)
