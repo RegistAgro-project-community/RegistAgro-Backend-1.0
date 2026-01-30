6d4e5da4b9332e26f480dde6342a9392c7423bc0
eucleniocadete <eucleniocadete@gmail.com>
2026-01-30 16:16:31 +0100

    Users update data path

---
+++ src/doc/paths/user-update.path.ts
@@ -0,0 +1,25 @@
+export const userUpdatePath = {
+    '/users/update': {
+        put: {
+            tags: ["Users"],
+            summary: "Atualizar dados",
+            requestBody: {
+                required: true,
+                content: {
+                    'application/json': {
+                        schema: {
+                            $ref: '#/components/schemas/UpdateData'
+                        }
+                    }
+                }
+            },
+            responses: {
+                200: {description: "OK"},
+                400: {description: "Bad Request"},
+                401: {description: "Unauthorized"},
+                403: {description: "Forbidden"},
+                404: {description: "Not Found"}
+            }
+        }
+    }
+}
