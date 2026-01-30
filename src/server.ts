79f20d02478052bdb83986b4b914819bf3aeb9df
eucleniocadete <eucleniocadete@gmail.com>
2026-01-30 16:52:48 +0100

    open RegistAgro documentation

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,16 @@
 import { app } from "./app";
 import { prisma } from "../lib/prisma";
+import open from "open";
 
 (
     async ()=>{
         const port = 5500
         try {
             await prisma.$connect()
+            await open(`http://localhost:${port}/docs`)
             app.listen(port, () =>{
                 console.log(`API: http://localhost:${port}`)
+                console.log(`Documentação: http://localhost:${port}/docs`)
             })
         } catch (error) {
             console.error("Falha na conexão com o servidor")
