7062e108c1ee0fd2cf7cc9d0f549efa071da83ad
eucleniocadete <eucleniocadete@gmail.com>
2026-02-05 18:46:50 +0100

    Reject order path

---
+++ src/doc/paths/reject-orders.ts
@@ -0,0 +1,29 @@
+export const rejectOrdersPath = {
+    '/orders/reject/order/{id}': {
+        patch: {
+            tags: ["Orders"],
+            summary: "Rejeitar pedidos de consumidores",
+            security: [
+                { bearerAuth: [] }
+            ],
+            parameters: [
+                {
+                    name: "id",
+                    in: "path",
+                    required: true,
+                    schema: {
+                        type: "string",
+                        example: "e6d2168a-4643-4ad5-aab7-c870a386aa17"
+                    }
+                }
+            ],
+            responses: {
+                200: {description: "OK"},
+                400: {description: "Bad Request"},
+                401: {description: "Unauthorized"},
+                403: {description: "Forbidden"},
+                404: {description: "Not Found"}
+            }
+        }
+    }
+}
