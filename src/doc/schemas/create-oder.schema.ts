b76e4922057ee8e1d164bc8883c8301731a1f6e8
eucleniocadete <eucleniocadete@gmail.com>
2026-02-05 18:48:34 +0100

    Create order schema

---
+++ src/doc/schemas/create-oder.schema.ts
@@ -0,0 +1,11 @@
+export const createOrderSchema = {
+    CreateOrder: {
+        type: "object",
+        required: ["name", "qtd", "unit"],
+        properties: {
+            name: {type: "string", example: "Banana"},
+            qtd: {type: "number", example: 10},
+            unit: {type: "string", example: "kg"}
+        }
+    }
+}
