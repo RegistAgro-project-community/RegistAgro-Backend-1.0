198f2464a29576fbe0d212672c502c1ecffac66c
eucleniocadete <eucleniocadete@gmail.com>
2026-02-10 16:56:04 +0100

    Change name photo name

--- src/utils/upload.ts
+++ src/utils/upload.ts
@@ -8,7 +8,7 @@ export interface uploadResult {
 }
 
 export async function upload(img: UploadedFile, format: string, url: string, to: "user" | "product"): Promise<uploadResult> {
-    const rename = `${to}-${dayjs().format('DD-MM-YYYY_HHmmss')}.${format}`
+    const rename = `${to}-${dayjs().format('DDMMYYYY_HHmmss')}.${format}`
 
     const pathUrl = path.join(url, rename)
     
