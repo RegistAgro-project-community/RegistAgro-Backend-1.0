075180c7f920091a9490be68574aedea136cebb7
eucleniocadete <eucleniocadete@gmail.com>
2026-02-11 11:52:35 +0100

    Add data object

--- src/doc/schemas/create-product.ts
+++ src/doc/schemas/create-product.ts
@@ -1,15 +1,22 @@
 export const createProductSchema = {
     CreateProduct: {
         type: "object",
-        required: ["name", "description", "price", "stock", "unit", "type", "transport"],
+        required: ["img", "data"],
         properties: {
-            name: {type: "string", example: "Banana"},
-            description: {type: "string", example: "A melhor banana de Luanda"},
-            price: {type: "number", example: 15200},
-            stock: {type: "number", example: 30},
-            unit: {type: "string", example: "t"},
-            type: {type: "string", example: "frutas"},
-            transport: {type: "string", example: "frigorifico"}
+            img: {type: "string", format: "binary", description: "Adicionar foto ao produto"},
+            data: {
+                type: "object",
+                required: ["name", "description", "price", "stock", "unit", "type", "transport"],
+                properties: {
+                    name: {type: "string", example: "Banana"},
+                    description: {type: "string", example: "A melhor banana de Luanda"},
+                    price: {type: "number", example: 15200},
+                    stock: {type: "number", example: 30},
+                    unit: {type: "string", example: "t"},
+                    type: {type: "string", example: "frutas"},
+                    transport: {type: "string", example: "frigorifico"}
+                }
+            }
         }
     }
 }
