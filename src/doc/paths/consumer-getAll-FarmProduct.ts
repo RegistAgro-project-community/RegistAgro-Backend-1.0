743eb4e2d5df71c79369096dd37312e160d3acbe
eucleniocadete <eucleniocadete@gmail.com>
2026-02-15 00:39:42 +0100

    Consumer view all farm products

---
+++ src/doc/paths/consumer-getAll-FarmProduct.ts
@@ -0,0 +1,29 @@
+export const consumerGetAllProductPath = {
+    '/products/consumers/get/farm/{id}': {
+        get: {
+            tags: ["Products"],
+            summary: "Consumidor visualiza todos osproduto de uma fazenda",
+            security: [
+                { bearerAuth: [] }
+            ],
+            parameters: [
+                {
+                    name: "id",
+                    in: "path",
+                    required: true,
+                    schema: {
+                        type: "string",
+                        example: "5477da12-e907-4e4c-a4ed-23cadba77b3e"
+                    }
+                }
+            ],
+            responses: {
+                200: {description: "ÕK"},
+                400: {description: "Bad Request"},
+                401: {description: "Unauthorized"},
+                403: {description: "Forbidden"},
+                404: {description: "Not Found"}
+            }
+        }
+    }
+}
