befcf8b8271da9fadcea4ee556a0a5dc566faeda
eucleniocadete <eucleniocadete@gmail.com>
2026-02-15 00:40:30 +0100

    Add Consumer view all registagro products products path

---
+++ src/doc/paths/consumer-getAll-products.ts
@@ -0,0 +1,18 @@
+export const consumerGetEveryProductPath = {
+    '/products/consumers/get/products': {
+        get: {
+            tags: ['Products'],
+            summary: "Consumidores vêm todos os produtos da plataforma",
+            security: [
+                { bearerAuth: [] }
+            ],
+            responses: {
+                200: {description: "OK"},
+                400: {description: "Bad Request"},
+                401: {description: "Unauthorized"},
+                403: {description: "Forbidden"},
+                404: {description: "Not Found"}
+            }
+        }
+    }
+}
