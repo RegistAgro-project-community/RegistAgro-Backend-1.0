50a7c198e68f7bb52f18c3eed8bf8af18e25df59
eucleniocadete <eucleniocadete@gmail.com>
2026-02-20 13:59:48 +0100

    Move responses inside of get method

--- src/doc/paths/verifyAcessToken.ts
+++ src/doc/paths/verifyAcessToken.ts
@@ -5,12 +5,12 @@ export const verifyAcessTokenPath = {
             summary: "Verifica se o token de acesso é válido",
             security: [
                 { bearerAuth: [] }
-            ]
-        },
-        responses: {
-            200: {description: "OK"},
-            401: {description: "Unauthorized"},
-            403: {description: "Forbidden"}
+            ],
+            responses: {
+                200: {description: "OK"},
+                401: {description: "Unauthorized"},
+                403: {description: "Forbidden"}
+            }
         }
     }
 }
