d78ab5fc7dbd33b7165fe1c99b10172aaf8ce68a
eucleniocadete <eucleniocadete@gmail.com>
2026-02-20 13:27:53 +0100

    Add local upload and upload to supabase storage

--- src/utils/upload.ts
+++ src/utils/upload.ts
@@ -1,6 +1,8 @@
 import dayjs from "dayjs";
 import type { UploadedFile } from "express-fileupload";
 import path from "node:path";
+import 'dotenv/config'
+import { supabase } from "../../lib/supabase";
 
 export interface uploadResult {
     ok: boolean,
@@ -10,15 +12,34 @@ export interface uploadResult {
 export async function upload(img: UploadedFile, format: string, url: string, to: "user" | "product"): Promise<uploadResult> {
     const rename = `${to}-${dayjs().format('DDMMYYYY_HHmmss')}.${format}`
 
-    const pathUrl = path.join(url, rename)
+    const pathUrl = process.env.ENV == 'dev' ? path.join(url, rename) : `${url}/${rename}`
     
-    return new Promise((resolve, reject)=>{
-        img.mv(pathUrl, error =>{
+    return new Promise( async (resolve, reject)=>{
+        if(process.env.ENV == 'dev'){
+            img.mv(pathUrl, error =>{
+                if(error){
+                    reject("Não foi possível salvar imagem")
+                }else{
+                    resolve({ok: true, filename: rename})
+                }
+            })
+        }else{
+            const { data, error } = await supabase.storage.from('Registagro').upload(pathUrl, img.data, {
+                contentType: img.mimetype,
+                upsert: false
+            })
+    
             if(error){
                 reject("Não foi possível salvar imagem")
             }else{
-                resolve({ok: true, filename: rename})
+                const { data: publicUrlData } = supabase.storage.from('Registagro').getPublicUrl(pathUrl)
+    
+                const publiUrl = publicUrlData.publicUrl
+    
+                resolve({ok: true, filename: publiUrl})
             }
-        })
+
+        }
+
     })
 }
