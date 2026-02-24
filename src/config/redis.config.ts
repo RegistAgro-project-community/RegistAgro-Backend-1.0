3719cce521d7dad5928c5945272c69a956ede528
eucleniocadete <eucleniocadete@gmail.com>
2026-02-24 03:04:16 +0100

    Redis enviroment config

--- src/config/redis.config.ts
+++ src/config/redis.config.ts
@@ -1,24 +1,32 @@
 import { createClient } from 'redis'
 import 'dotenv/config'
 
-export const redisClient = createClient({
-    username: process.env.REDIS_USER!,
-    password: process.env.REDIS_PASS!,
-    socket: {
-        host: 'redis-15935.crce214.us-east-1-3.ec2.cloud.redislabs.com',
-        port: 15935
+var redisClient = createClient()
+
+if(process.env.ENV == "dev"){
+    redisClient.on('error', err => console.error('Redis Client Error'))
+}else {
+    redisClient = createClient({
+        username: process.env.REDIS_USER!,
+        password: process.env.REDIS_PASS!,
+        socket: {
+            host: 'redis-15935.crce214.us-east-1-3.ec2.cloud.redislabs.com',
+            port: 15935
+        }
+    });
+
+    redisClient.on('error', err => console.error('Redis Client Error'))
+
+    try {
+        await redisClient.connect()
+        
+        await redisClient.set('foo', 'bar')
+        const result = await redisClient.get('foo')
+        console.log(result)  
+        
+    } catch (error) {
+        console.error("Erro no Redis Client")
     }
-});
-
-redisClient.on('error', err => console.error('Redis Client Error'))
-
-try {
-    await redisClient.connect()
-    
-    await redisClient.set('foo', 'bar')
-    const result = await redisClient.get('foo')
-    console.log(result)  
-    
-} catch (error) {
-    console.error("Erro no Redis Client")
 }
+
+export { redisClient }
