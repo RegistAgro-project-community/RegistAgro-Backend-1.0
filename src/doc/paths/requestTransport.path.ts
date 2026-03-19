e65fcf94db97cfe7578defc265a5184b42ebd2a3
eucleniocadete <eucleniocadete@gmail.com>
2026-03-19 16:02:52 +0100

    Request transport path

---
+++ src/doc/paths/requestTransport.path.ts
@@ -0,0 +1,29 @@
+export const requestTransportPath = {
+    "/transports/request": {
+        post: {
+            tags: ["Transports"],
+            summary: "Contratar transportadora pelo seu veículo",
+            security: [
+                {bearerAuth: {}}
+            ],
+            requestBody: {
+                required: true,
+                content: {
+                    'application/json': {
+                        schema: {
+                            $ref: "#/components/schemas/RequestTransport"
+                        }
+                    }
+                }
+            },
+            responses: {
+                201: {description: "Created"},
+                400: {description: "Bad Request"},
+                401: {description: "Unauthorized"},
+                403: {description: "Forbidden"},
+                404: {description: "Not Found"},
+                409: {description: "Conflict"}
+            }
+        }
+    }
+}
