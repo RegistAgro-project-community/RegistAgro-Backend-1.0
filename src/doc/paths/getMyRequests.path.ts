0f6b4f38d28badfe7a3ecd9a4cc22579467f47fa
eucleniocadete <eucleniocadete@gmail.com>
2026-03-20 21:10:21 +0100

    get carriers transport requests path

---
+++ src/doc/paths/getMyRequests.path.ts
@@ -0,0 +1,18 @@
+export const getMyRequestsPath = {
+    '/transports/carrier/request/get': {
+        get: {
+            tags: ["Transports"],
+            summary: "Transportadoras visualizam suas solicitações de transporte",
+            security: [
+                {bearerAuth: {}}
+            ],
+            responses: {
+                200: {description: "OK"},
+                400: {description: "Bad Request"},
+                401: {description: "Unauthorized"},
+                403: {description: "Forbidden"},
+                404: {description: "Not Found"}
+            }
+        }
+    }
+}
