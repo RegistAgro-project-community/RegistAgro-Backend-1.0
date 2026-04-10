f0b860de1d95e8218fd5f01dc9a3e86853534d92
eucleniocadete <eucleniocadete@gmail.com>
2026-04-10 13:43:44 +0100

    Add new middlewares

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -1,7 +1,7 @@
 import { Router } from "express";
 import authController from "../controllers/auth/auth.controller.js";
 import { auth } from "../middlewares/auth.js";
-import { authorization } from "../middlewares/permissions.js";
+import { authorization } from "../middlewares/authorization.js";
 import productsController from "../controllers/products.controller.js";
 import usersController from "../controllers/users.controller.js";
 import ordersController from "../controllers/orders.controller.js";
@@ -9,6 +9,8 @@ import { healthyRoute } from "../controllers/healthy.controller.js";
 import companyController from "../controllers/company.controller.js";
 import paymentController from "../controllers/payment.controller.js";
 import transportController from "../controllers/transport.controller.js";
+import { permission } from "../middlewares/permision.js";
+import { validAccount } from "../middlewares/validAccount.js";
 
 const router = Router()
 
@@ -30,37 +32,38 @@ router.post('/auth/farm/login', authController.farmSignIn)
 router.post('/auth/login', authController.login)
 
 //Products
-router.post('/products/create', auth, authorization("farm", "create"), productsController.create)
-router.patch('/products/upload/product/:id', auth, authorization("farm", "update"), productsController.productPhoto)
-router.put('/products/update/product/:id', auth, authorization("farm", "update"), productsController.update)
-router.get('/products/farms/get/products', auth, authorization("farm", "read"), productsController.getAll)
-router.get('/products/farms/get/product/:id', auth, authorization("farm", "read"), productsController.get)
-router.delete('/products/delete/product/:id', auth, authorization("farm", "delete"), productsController.delete)
-router.get('/products/consumers/get/farm/:id', auth, authorization("consumer", "read"), productsController.consumerReadAll)
-router.get('/products/consumers/product/:id', auth, authorization("consumer", "read"), productsController.consumerRead)
-router.get('/products/consumers/get/products', auth, authorization("consumer", "read"), productsController.consumerGetAll)
+router.post('/products/create', auth, validAccount(), authorization("farm", "create"), productsController.create)
+router.patch('/products/upload/product/:id', auth, validAccount(), authorization("farm", "update"), productsController.productPhoto)
+router.put('/products/update/product/:id', auth, validAccount(), authorization("farm", "update"), productsController.update)
+router.get('/products/farms/get/products', auth, validAccount(), authorization("farm", "read"), permission("farm"), productsController.getAll)
+router.get('/products/farms/get/product/:id', auth, validAccount(), authorization("farm", "read"), permission("farm"), productsController.get)
+router.delete('/products/delete/product/:id', auth, validAccount(), authorization("farm", "delete"), productsController.delete)
+router.get('/products/consumers/get/farm/:id', auth, validAccount(), authorization("consumer", "read"), permission("consumer"), productsController.consumerReadAll)
+router.get('/products/consumers/product/:id', auth, validAccount(), authorization("consumer", "read"), permission("consumer"), productsController.consumerRead)
+router.get('/products/consumers/get/products', auth, validAccount(), authorization("consumer", "read"), permission("consumer"), productsController.consumerGetAll)
 
 //Users
-router.put('/users/update', auth, usersController.update)
-router.delete('/users/delete', auth, usersController.delete)
-router.post('/users/upload/profile', auth, usersController.profilePhoto)
-router.get('/users/profile', auth, usersController.profile)
+router.put('/users/update', auth, validAccount(), usersController.update)
+router.delete('/users/delete', auth, validAccount(), usersController.delete)
+router.post('/users/upload/profile', auth, validAccount(),usersController.profilePhoto)
+router.get('/users/profile', auth, validAccount(), usersController.profile)
 
 //Orders
-router.post('/orders/create/farm/:id', auth, authorization("consumer", "create"), ordersController.create)
-router.patch('/orders/payment/confirm', auth, authorization("consumer", "update"), paymentController.confirm)
-router.get('/orders/farms/order/get', auth, authorization("farm", "read"), ordersController.viewsAll)
-router.patch('/orders/accept/order/:id', auth, authorization("farm", "update"), ordersController.accept)
-router.patch('/orders/reject/order/:id', auth, authorization("farm", "update"), ordersController.reject)
-router.get('/orders/consumers/order/sent', auth, authorization("consumer", "create"), ordersController.sentOrders)
-router.patch('/orders/consumers/cancel/order/:id', auth, authorization("consumer", "update"), ordersController.cancelOrder)
-router.put('/orders/consumers/update/order/:id', auth, authorization("consumer", "update"), ordersController.updateOrder)
-router.delete('/orders/consumers/delete/order/:id', auth, authorization("consumer", "delete"), ordersController.deleteOrder)
+router.post('/orders/create/farm/:id', auth, validAccount(), authorization("consumer", "create"), ordersController.create)
+router.patch('/orders/payment/confirm', auth, validAccount(), authorization("consumer", "update"), paymentController.confirm)
+router.get('/orders/farms/order/get', auth, validAccount(), authorization("farm", "read"), permission("farm"), ordersController.viewsAll)
+router.patch('/orders/accept/order/:id', auth, validAccount(), authorization("farm", "update"), ordersController.accept)
+router.patch('/orders/reject/order/:id', auth, validAccount(), authorization("farm", "update"), ordersController.reject)
+router.get('/orders/consumers/order/sent', auth, validAccount(), authorization("consumer", "read"), permission("consumer"), ordersController.sentOrders)
+router.patch('/orders/consumers/cancel/order/:id', auth, validAccount(), authorization("consumer", "update"), ordersController.cancelOrder)
+router.put('/orders/consumers/update/order/:id', auth, validAccount(), authorization("consumer", "update"), ordersController.updateOrder)
+router.delete('/orders/consumers/delete/order/:id', auth, validAccount(), authorization("consumer", "delete"), ordersController.deleteOrder)
 
 //Transports
-router.post('/transports/vehicle/create', auth, authorization("carrier", "create"), transportController.createTransport)
-router.get('/transports/farms/get/vehicle/:transport', auth, authorization("farm", "read"), transportController.showCarriers)
-router.post('/transports/request', auth, authorization("farm", "create"), transportController.hireCarrier)
-router.get('/transports/carriers/vehicles', auth, authorization("carrier", "read"), transportController.myVehicles)
-router.get('/transports/carrier/request/get', auth, authorization("carrier", "read"), transportController.getRequest)
+router.post('/transports/vehicle/create', auth, validAccount(), authorization("carrier", "create"), transportController.createTransport)
+router.get('/transports/farms/get/vehicle/:transport', auth, validAccount(), authorization("farm", "read"), permission("farm"), transportController.showCarriers)
+router.post('/transports/request', auth, validAccount(), authorization("farm", "create"), transportController.hireCarrier)
+router.get('/transports/carriers/vehicles', auth, validAccount(), authorization("carrier", "read"), permission("carrier"), transportController.myVehicles)
+router.get('/transports/carrier/request/get', auth, validAccount(), authorization("carrier", "read"), permission("carrier"), transportController.getRequest)
+
 export { router }
