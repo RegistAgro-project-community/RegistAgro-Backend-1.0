3c7da5ed8954780c650b050fb9c42364926af4cc
eucleniocadete <eucleniocadete@gmail.com>
2026-05-05 01:47:17 +0100

    Accept transport request PATH

---
+++ src/doc/paths/acceptRequest.path.ts
@@ -0,0 +1,28 @@
+export const acceptRequestPath = {
+    "/transports/carrier/request/accept": {
+        patch: {
+            tags: ["Transports"],
+            summary: "Aceitar pedido de transporte",
+            security: [
+                {bearerAuth: []}
+            ],
+            requestBody: {
+                required: true,
+                content: {
+                    'application/json': {
+                        schema: {
+                            $ref: "#/components/schemas/AcceptRequest"
+                        }
+                    }
+                }
+            },
+            responses: {
+                200: {description: "OK"},
+                400: {description: "Bad Request"},
+                401: {description: "Unauthorized"},
+                403: {description: "Forbidden"},
+                404: {description: "Not Found"}
+            }
+        }
+    }
+}
